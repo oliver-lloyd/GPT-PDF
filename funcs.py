9a080c2f5c42a8fb4d97f88289f5108d16e7191e
Ollie Lloyd <oliver.lloyd@bristol.ac.uk>
2024-05-05 18:11:43 +0100

    Function for saving, and additional user args

--- funcs.py
+++ funcs.py
@@ -1,13 +1,15 @@
 from openai import OpenAI
 
-def gpt_parse(raw: list, client: OpenAI):
+
+def gpt_parse(raw: list, client: OpenAI, model: str):
     system_content = 'You are an assistant that helps correctly parse strings. The strings are misformatted, with some spaces added randomly and others missing. You will accept one or more string(s) as input, correct these errors in the string(s), and return the result.'
     messages = [{"role": "system", "content": system_content}]
     messages += [{"role": "user", "content": raw_str} for raw_str in raw]
 
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages
+        model=model,
+        messages=messages,
+        seed=42
     )
     return completion
 
@@ -18,4 +20,22 @@ def analyse(content: str):
     words = []
     for line_ in lines:
         words += line_.split(' ')
-    return f'Line count: {len(lines)}. Word count: {len(words)}'
+    result = {
+        'num_lines': len(lines),
+        'num_words': len(words)
+    }
+    return result
+
+
+def write_result(result: dict, out_path: str):
+    from datetime import datetime
+    from os.path import exists
+
+    result['timestamp'] = datetime.now().timestamp()
+    mode = 'a' if exists(out_path) else 'w'
+    keys = result.keys()
+    with open(out_path, mode) as f:
+        if mode == 'w':
+            f.write(','.join(keys) + '\n')
+        f.write(','.join(str(result[key]) for key in keys) + '\n')
+
--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from PyPDF2 import PdfReader
 from dotenv import dotenv_values
 from openai import OpenAI
 from argparse import ArgumentParser
-from funcs import gpt_parse, analyse
+from funcs import gpt_parse, analyse, write_result
 
 parser = ArgumentParser()
 parser.add_argument('PDF_path')
+parser.add_argument('--result_path', '-r')
+parser.add_argument('--model', '-m', default='gpt-4')
 args = parser.parse_args()
 
 # Read in file
+print('Loading PDF..')
 reader = PdfReader(args.PDF_path)
 pages_raw = [page.extract_text() for page in reader.pages]
 pages = [' '.join(page.split('\n')) for page in pages_raw]
@@ -20,10 +23,19 @@ client = OpenAI(
 )
 
 # Parse the text
-completion = gpt_parse(pages, client)
+print('Parsing with ChatGPT. This may take a few seconds..')
+completion = gpt_parse(pages, client, args.model)
 parsed = completion.choices[0].message.content
 
-# Analyse text
+# Analyse
+print('Analysing text..')
 out = analyse(parsed)
-print(out)
-quit()
+
+# Save results or print to console
+print('Saving..')
+out['file_path'] = args.PDF_path
+out['model'] = args.model
+if args.result_path:
+    write_result(out, args.result_path)
+else:
+    print(out)
